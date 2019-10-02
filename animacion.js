2432aa4a2680961607a1b50784eb5e0c1241d8b5
lisandro <lisandrodiomedes@gmail.com>
2019-10-02 19:50:06 -0400

    Animaciones añadidas

---
+++ animacion.js
@@ -0,0 +1,41 @@
+window.sr = ScrollReveal();
+sr.reveal('.menu',{
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+})
+
+sr.reveal('.mover',{
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+})
+
+
+sr.reveal('.contenedor-textos-nuestrosProductos',{
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+})
+
+sr.reveal('.contenedor-galeria-portafolio',{
+    duration: 3000,
+    origin: 'top',
+    distance: '300px'
+})
+sr.reveal('.cards',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px'
+})
+sr.reveal('.contenedor-nuestros-servicios',{
+    duration: 3000,
+    origin: 'top',
+    distance: '300px'
+})
+
+sr.reveal('.contenedor-footer',{
+    duration: 1000,
+    origin: 'bottom',
+    distance: '300px'
+})
